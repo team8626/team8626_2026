b6cf4fa2c0d7658b6b369d8289a5b7ad518d874b
Cam <cybersailors@team8626.com>
2026-02-19 18:08:51 -0500

    Finished first implementation of IndexAndShootCommand

--- src/main/java/frc/robot/commands/IndexAndShootCommand.java
+++ src/main/java/frc/robot/commands/IndexAndShootCommand.java
@@ -1,13 +1,18 @@
 /** THIS COMMAND IS NOT COMPLETED YET */
 /** WILL BE UPDATED IN FUTURE COMMITS */
-
 package frc.robot.commands;
 
 import static edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.*;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.shooter.Shooter;
@@ -17,11 +22,19 @@ public class IndexAndShootCommand extends Command {
   private final Indexer indexer;
   private final Drive drive;
 
+  private final double h = 0;
   private final double flywheelRadiousInInches = 2.0; // TODO: find actual value
-  private final Angle shooterAngle = Degrees.of(38.0);
-  private final Distance shooterXPosition = Inches.of(0.0);
-  private final Distance shooterYPosition = Inches.of(0.0);
-  private final Distance shooterZPosition = Inches.of(15);
+  // private final Angle shooterAngle = Degrees.of(68.0);//TODO: TAKE ANGLE INTO ACCOUNT IN
+  // CALCULATIONS
+  private final Distance hubHeight = Inches.of(80.0);
+  private final Translation2d hubPosition =
+      new Translation2d(Inches.of(181.56), Inches.of(316.64 / 2));
+  private final Transform3d shootertoRobotCenter =
+      new Transform3d(
+          Inches.of(-8),
+          Inches.of(4.5),
+          Inches.of(16.25),
+          new Rotation3d(new Rotation2d(Degrees.of(0))));
 
   public IndexAndShootCommand(Shooter shooter, Indexer indexer, Drive drive) {
     this.shooter = shooter;
@@ -32,23 +45,44 @@ public class IndexAndShootCommand extends Command {
 
   @Override
   public void initialize() {
-    new SequentialCommandGroup(
-        new ShooterRampupCommand(shooter),
-        IndexerCommands.runForDuration(
-            indexer,
-            1.2) // TODO: Link, make an IndexerCommand.run that starts the indexer and keeps it on
-        // until a stop command is called
-        );
+    shooter.runVelocity(getShooterVelocity(drive.getPose()));
   }
 
   @Override
-  public void execute() {}
+  public void execute() {
+    // If shooter is fast enough, run indexer to feed balls into shooter
+    if (shooter.isAtGoal()) {
+      indexer.runVelocity(RPM.of(300)); // TODO: find actual value for indexer velocity
+    }
+    shooter.runVelocity(getShooterVelocity(drive.getPose()));
+  }
 
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    shooter.stop();
+    indexer.stop();
+  }
 
   @Override
   public boolean isFinished() {
     return false; // TODO: implement actual logic to determine when finished
+  }
+
+  private AngularVelocity getShooterVelocity(Pose2d robotPosition) {
+    /** ALL UNITS IN THIS METHOD ARE IN FEET */
+    Pose3d shooterPose3d =
+        new Pose3d(robotPosition)
+            .plus(shootertoRobotCenter); // TODO: verify this transformation is correct
+    double Xs =
+        Units.metersToFeet(
+            shooterPose3d.getTranslation().toTranslation2d().getDistance(hubPosition));
+    double Y0 = 0;
+    double Ys = Y0 + Units.metersToFeet(shootertoRobotCenter.getZ());
+    double Vy = Math.sqrt((h * h) + ((hubHeight.in(Foot) / 12) - Ys) * 32 * 2);
+    double tm = (Vy - h) / 32;
+    double Vx = Xs / tm;
+    double v = Math.sqrt((Vy * Vy) + (Vx * Vx));
+    double vRPM = (v * 12) / (Math.PI * flywheelRadiousInInches) * 60;
+    return RPM.of(vRPM);
   }
 }
