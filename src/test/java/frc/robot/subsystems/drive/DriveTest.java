c7e5e9ffb32d4a16d1a6888f9bd43fed2b6a87e4
Cyber Sailors <team8626.sw@gmail.com>
2026-02-19 09:13:24 -0500

    Move TeleopDrive tests to their own test file

--- src/test/java/frc/robot/subsystems/drive/DriveTest.java
+++ src/test/java/frc/robot/subsystems/drive/DriveTest.java
@@ -24,7 +24,6 @@ import edu.wpi.first.hal.HAL;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import frc.robot.commands.TeleopDrive;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -322,72 +321,5 @@ public class DriveTest {
     assertTrue(Math.abs(frModuleIO.lastDriveVelocitySetpoint) <= maxModuleSpeed + DELTA);
     assertTrue(Math.abs(blModuleIO.lastDriveVelocitySetpoint) <= maxModuleSpeed + DELTA);
     assertTrue(Math.abs(brModuleIO.lastDriveVelocitySetpoint) <= maxModuleSpeed + DELTA);
-  }
-
-  @Test
-  void testBumpZoneAngle() {
-    assertTrue(TeleopDrive.getBumpLockAngle(Rotation2d.fromDegrees(5.0)).getDegrees() == 45.0);
-    assertTrue(TeleopDrive.getBumpLockAngle(Rotation2d.fromDegrees(-5.0)).getDegrees() == -45.0);
-    assertTrue(TeleopDrive.getBumpLockAngle(Rotation2d.fromDegrees(175.0)).getDegrees() == 135.0);
-    assertTrue(TeleopDrive.getBumpLockAngle(Rotation2d.fromDegrees(-175.0)).getDegrees() == -135.0);
-  }
-
-  @Test
-  void testTrenchZoneAngle() {
-    assertTrue(TeleopDrive.getTrenchLockAngle(Rotation2d.fromDegrees(100.0)).getDegrees() == 180.0);
-    assertTrue(
-        TeleopDrive.getTrenchLockAngle(Rotation2d.fromDegrees(-100.0)).getDegrees() == -180.0);
-    assertTrue(TeleopDrive.getTrenchLockAngle(Rotation2d.fromDegrees(80.0)).getDegrees() == 0.0);
-    assertTrue(TeleopDrive.getTrenchLockAngle(Rotation2d.fromDegrees(-80.0)).getDegrees() == 0.0);
-  }
-
-  @Test
-  void testBumpTrigger() {
-    TeleopDrive teleopDrive = new TeleopDrive(drive, null);
-
-    // Left bump zone - Current Alliance
-    drive.setPose(new Pose2d(4.0, 5.5, new Rotation2d()));
-    assertTrue(teleopDrive.inBumpZone());
-
-    // Right bump zone - Current Alliance
-    drive.setPose(new Pose2d(4.0, 2.5, new Rotation2d()));
-    assertTrue(teleopDrive.inBumpZone());
-
-    // Left bump zone - Opposite Alliance
-    drive.setPose(new Pose2d(12.0, 5.5, new Rotation2d()));
-    assertTrue(teleopDrive.inBumpZone());
-
-    // Right bump zone - Opposite Alliance
-    drive.setPose(new Pose2d(12.0, 2.5, new Rotation2d()));
-    assertTrue(teleopDrive.inBumpZone());
-
-    // Outside bump zones
-    drive.setPose(new Pose2d(8.0, 5.5, new Rotation2d()));
-    assertTrue(!teleopDrive.inBumpZone());
-  }
-
-  @Test
-  void testTrenchTrigger() {
-    TeleopDrive teleopDrive = new TeleopDrive(drive, null);
-
-    // Left trench zone - Current Alliance
-    drive.setPose(new Pose2d(3.5, 7.5, new Rotation2d()));
-    assertTrue(teleopDrive.inTrenchZone());
-
-    // Right trench zone - Current Alliance
-    drive.setPose(new Pose2d(4.50, .75, new Rotation2d()));
-    assertTrue(teleopDrive.inTrenchZone());
-
-    // Left trench zone - Opposite Alliance
-    drive.setPose(new Pose2d(12.0, 7.5, new Rotation2d()));
-    assertTrue(teleopDrive.inTrenchZone());
-
-    // Right trench zone - Opposite Alliance
-    drive.setPose(new Pose2d(12.0, 0.75, new Rotation2d()));
-    assertTrue(teleopDrive.inTrenchZone());
-
-    // Outside trench zones
-    drive.setPose(new Pose2d(8.0, 5.5, new Rotation2d()));
-    assertTrue(!teleopDrive.inTrenchZone());
   }
 }
