b7462bf62cfa53423198d55e718bb1b4e7e7b9d7
Cam <cybersailors@team8626.com>
2026-03-15 17:00:55 -0400

    added todo

--- src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
@@ -30,7 +30,8 @@ public class ClimberConstants {
   public static final Voltage ZERO_VOLTAGE = Volts.of(-1);
   public static final Voltage CLIMB_LOCK_VOLTAGE = Volts.of(-0.5);
 
-  public static final Current STALL_CURRENT = Amps.of(20);
+  public static final Current STALL_CURRENT =
+      Amps.of(20); // TODO: adjust this value to prevent damage
   public static final AngularVelocity STALL_ANGULAR_VELOCITY = RadiansPerSecond.of(6);
 
   // TODO: Tuned Values
