8ee85d459ead1c91929c374258b945830290bd7d
Cyber Sailors <team8626.sw@gmail.com>
2026-03-21 15:44:57 -0400

    fix: update Blurp and unJam speeds

--- src/main/java/frc/robot/subsystems/intakeLinkage/IntakeLinkageConstants.java
+++ src/main/java/frc/robot/subsystems/intakeLinkage/IntakeLinkageConstants.java
@@ -31,8 +31,8 @@ public class IntakeLinkageConstants {
   public static final Angle AGITAGE_ANGLE = Degrees.of(110);
   public static final Angle DEPLOY_ANGLE = Degrees.of(192);
   public static final Angle PLOW_ANGLE = Degrees.of(210);
-  public static final Angle BLURP_ANGLE = Degrees.of(150);
-  public static final Angle HOPPER_OPEN_ANGLE = Degrees.of(175);
+  public static final Angle BLURP_ANGLE = Degrees.of(175);
+  public static final Angle HOPPER_OPEN_ANGLE = Degrees.of(145);
 
   public static final Angle STARTING_ANGLE = STOW_ANGLE;
 
