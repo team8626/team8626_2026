78c515a9f2ea7fe1988723b2b65a8b201ddd6aba
Cyber Sailors <team8626.sw@gmail.com>
2026-03-22 23:20:21 -0400

    refactor: remove unused trench and bump zone logic from TeleopDriveCommand and its tests

--- src/main/java/frc/robot/commands/TeleopDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleopDriveCommand.java
@@ -9,7 +9,6 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -27,7 +26,6 @@ import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 import org.littletonrobotics.frc2026.FieldConstants;
 import org.littletonrobotics.junction.AutoLogOutput;
-import org.littletonrobotics.junction.Logger;
 
 /** Default drive command to run that drives based on controller input */
 public class TeleopDriveCommand extends Command {
@@ -46,28 +44,18 @@ public class TeleopDriveCommand extends Command {
   private double rotSpeedToTarget = 0.0;
 
   @AutoLogOutput private DriveSpeed driveSpeed = DriveSpeed.DEFAULT;
-
-  @AutoLogOutput
-  private final Trigger inTrenchZoneTrigger = new Trigger(this::inTrenchZone).debounce(0.1);
-
-  @AutoLogOutput
-  private final Trigger inBumpZoneTrigger = new Trigger(this::inBumpZone).debounce(0.1);
-
   @AutoLogOutput private boolean forceTargetTracking = false;
   @AutoLogOutput private boolean forceTargetLockThenX = false;
 
-  @AutoLogOutput private final Trigger hubTrackTrigger = new Trigger(() -> forceTargetTracking);
-  @AutoLogOutput private final Trigger hubAimTrigger = RobotContainer.getAimTrigger();
+  @AutoLogOutput private final Trigger targetTrackTrigger = new Trigger(() -> forceTargetTracking);
+  @AutoLogOutput private final Trigger targetAimTrigger = RobotContainer.getAimTrigger();
 
   private Supplier<Translation3d> targetSupplier = HUB_TARGET_SUPPLIER;
 
-  private final TunablePIDController trenchYController =
-      new TunablePIDController(DriveConstants.TRENCH_TRANSLATION_CONSTANTS);
   private final TunablePIDController rotationController =
       new TunablePIDController(DriveConstants.ROTATION_CONSTANTS);
 
   @AutoLogOutput private DriveMode currentDriveMode = DriveMode.NORMAL;
-  @AutoLogOutput private double trenchLockAngle = 9999;
 
   public TeleopDriveCommand(AkitDrive drive, CommandXboxController controller) {
     this.drive = drive;
@@ -77,41 +65,25 @@ public class TeleopDriveCommand extends Command {
     this.driveLimiter =
         new SlewRateLimiter2d(DriveConstants.MAX_TELEOP_ACCEL.in(MetersPerSecondPerSecond));
 
-    inTrenchZoneTrigger.onTrue(updateDriveMode(DriveMode.TRENCH_LOCK));
-    inBumpZoneTrigger.onTrue(updateDriveMode(DriveMode.BUMP_LOCK));
-    inTrenchZoneTrigger
-        .or(inBumpZoneTrigger)
-        .onFalse(
-            Commands.runOnce(
-                () ->
-                    currentDriveMode =
-                        (hubTrackTrigger.getAsBoolean()
-                            ? DriveMode.TARGET_TRACK
-                            : DriveMode.NORMAL)));
-
     RobotContainer.getTrackTrigger()
         .onTrue(Commands.runOnce(() -> forceTargetTracking = !forceTargetTracking));
 
-    hubTrackTrigger
+    targetTrackTrigger
         .onTrue(updateDriveMode(DriveMode.TARGET_TRACK))
         .onFalse(updateDriveMode(DriveMode.NORMAL));
 
-    if (hubAimTrigger != null) {
-      hubAimTrigger
+    if (targetAimTrigger != null) {
+      targetAimTrigger
           .whileTrue(updateDriveMode(DriveMode.TARGET_AIM))
           .onFalse(
               Commands.runOnce(
                   () ->
                       currentDriveMode =
-                          hubTrackTrigger.getAsBoolean()
+                          targetTrackTrigger.getAsBoolean()
                               ? DriveMode.TARGET_TRACK
                               : DriveMode.NORMAL));
     }
 
-    for (int i = 0; i < 4; i++) {
-      Logger.recordOutput("Trench" + i, TeleopDriveConstants.TRENCH_ZONES[i]);
-      Logger.recordOutput("Bump" + i, TeleopDriveConstants.BUMP_ZONES[i]);
-    }
     addRequirements(drive);
   }
 
@@ -123,56 +95,6 @@ public class TeleopDriveCommand extends Command {
     return new Pose2d(new Translation2d(), linearDirection)
         .transformBy(new Transform2d(linearMagnitude, 0.0, new Rotation2d()))
         .getTranslation();
-  }
-
-  public boolean inTrenchZone() {
-    Pose2d robotPose = drive.getPose();
-    for (Translation2d[] zone : TeleopDriveConstants.TRENCH_ZONES) {
-      if (robotPose.getX() >= zone[0].getX()
-          && robotPose.getX() <= zone[1].getX()
-          && robotPose.getY() >= zone[0].getY()
-          && robotPose.getY() <= zone[1].getY()) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private Distance getTrenchY() {
-    Pose2d robotPose = drive.getPose();
-    if (robotPose.getMeasureY().gte(TeleopDriveConstants.FIELD_WIDTH.div(2))) {
-      return TeleopDriveConstants.FIELD_WIDTH.minus(TeleopDriveConstants.TRENCH_CENTER);
-    }
-    return TeleopDriveConstants.TRENCH_CENTER;
-  }
-
-  public static Rotation2d getTrenchLockAngle(Rotation2d currentRotation) {
-    if (Math.abs(MathUtil.inputModulus(currentRotation.getDegrees(), -180, 180)) > 90) {
-      return Rotation2d.fromDegrees(Math.copySign(180, currentRotation.getDegrees()));
-    }
-    return Rotation2d.kZero;
-  }
-
-  public static Rotation2d getBumpLockAngle(Rotation2d currentRotation) {
-    for (int i = -135; i < 180; i += 90) {
-      if (Math.abs(MathUtil.inputModulus(currentRotation.getDegrees() - i, -180, 180)) <= 45) {
-        return Rotation2d.fromDegrees(i);
-      }
-    }
-    return Rotation2d.kZero;
-  }
-
-  public boolean inBumpZone() {
-    Pose2d robotPose = drive.getPose();
-    for (Translation2d[] zone : TeleopDriveConstants.BUMP_ZONES) {
-      if (robotPose.getX() >= zone[0].getX()
-          && robotPose.getX() <= zone[1].getX()
-          && robotPose.getY() >= zone[0].getY()
-          && robotPose.getY() <= zone[1].getY()) {
-        return true;
-      }
-    }
-    return false;
   }
 
   private Command updateDriveMode(DriveMode driveMode) {
@@ -190,7 +112,6 @@ public class TeleopDriveCommand extends Command {
 
   @Override
   public void execute() {
-    // Forcing the drive to X
     boolean shouldStopWithX =
         forceTargetLockThenX && ShooterCommandsUtil.inPositionToShoot(drive, targetSupplier.get());
     if (shouldStopWithX) {
@@ -204,7 +125,7 @@ public class TeleopDriveCommand extends Command {
     linearVelocity = driveLimiter.calculate(linearVelocity);
 
     switch (currentDriveMode) {
-      case NORMAL, TRENCH_LOCK, BUMP_LOCK:
+      case NORMAL:
         double omega =
             MathUtil.applyDeadband(omegaSupplier.getAsDouble(), ControllerConstants.DEADBAND);
         omega = Math.copySign(omega * omega, omega);
@@ -323,8 +244,6 @@ public class TeleopDriveCommand extends Command {
 
   private enum DriveMode {
     NORMAL,
-    TRENCH_LOCK,
-    BUMP_LOCK,
     TARGET_TRACK,
     TARGET_AIM
   }
