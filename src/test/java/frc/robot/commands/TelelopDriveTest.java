e6f41e0fe789096faeaf96285d8872327c155201
Cyber Sailors <team8626.sw@gmail.com>
2026-03-22 23:41:31 -0400

    spotless

--- src/test/java/frc/robot/commands/TelelopDriveTest.java
+++ src/test/java/frc/robot/commands/TelelopDriveTest.java
@@ -1,10 +1,8 @@
 package frc.robot.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import edu.wpi.first.hal.HAL;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
